c28359c95a0a8034b3d26586d07ee930c35e1e94
ROOT <junroot0909@gmail.com>
2022-01-06 22:14:15 +0900

    test: 컴퓨터소프트웨어학부 관련 TC 작성

--- post-collector/src/test/java/postcollector/presentation/postcollector/CsPostCollectorTest.java
+++ post-collector/src/test/java/postcollector/presentation/postcollector/CsPostCollectorTest.java
@@ -2,10 +2,17 @@ package postcollector.presentation.postcollector;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import postcollector.domain.Post;
 import postcollector.presentation.postcollector.cs.CsPostCollector;
 
@@ -14,10 +21,50 @@ class CsPostCollectorTest {
 
     @DisplayName("모든 게시글 수집")
     @Test
-    void collectNewPosts() {
-        CsPostCollector csPostCollector = new CsPostCollector();
-        List<Post> posts = csPostCollector.collectNewPosts(LocalDate.of(2022, 1, 1));
+    void collectNewPosts() throws IOException {
+        File page1File = new File("./src/test/resources/cs_page1.html");
+        File page2File = new File("./src/test/resources/cs_page2.html");
+        Document document1 = Jsoup.parse(page1File, "euc-kr");
+        Document document2 = Jsoup.parse(page2File, "euc-kr");
+        try (MockedStatic<Jsoup> jsoup = Mockito.mockStatic(Jsoup.class)) {
+            jsoup.when(() -> Jsoup.connect(
+                    "http://cs.hanyang.ac.kr/board/info_board.php?ptype=&page=1&code=notice"))
+                .thenReturn(new CustomConnection(document1));
+            jsoup.when(() -> Jsoup.connect(
+                    "http://cs.hanyang.ac.kr/board/job_board.php?ptype=&page=1&code=job_board"))
+                .thenReturn(new CustomConnection(document1));
+            jsoup.when(() -> Jsoup.connect(
+                    "http://cs.hanyang.ac.kr/board/gradu_board.php?ptype=&page=1&code=gradu_board"))
+                .thenReturn(new CustomConnection(document1));
 
-        assertThat(posts.size() > 0).isTrue();
+            jsoup.when(() -> Jsoup.connect(
+                    "http://cs.hanyang.ac.kr/board/info_board.php?ptype=&page=2&code=notice"))
+                .thenReturn(new CustomConnection(document2));
+            jsoup.when(() -> Jsoup.connect(
+                    "http://cs.hanyang.ac.kr/board/job_board.php?ptype=&page=2&code=job_board"))
+                .thenReturn(new CustomConnection(document2));
+            jsoup.when(() -> Jsoup.connect(
+                    "http://cs.hanyang.ac.kr/board/gradu_board.php?ptype=&page=2&code=gradu_board"))
+                .thenReturn(new CustomConnection(document2));
+
+            CsPostCollector csPostCollector = new CsPostCollector();
+            List<Post> posts = csPostCollector.collectNewPosts(LocalDate.of(2021, 11, 24));
+
+            assertThat(posts).hasSize(12);
+        }
+    }
+
+    private static class CustomConnection extends HttpConnection {
+
+        private final Document document;
+
+        public CustomConnection(final Document document) {
+            this.document = document;
+        }
+
+        @Override
+        public Document get() {
+            return document;
+        }
     }
 }
---
+++ post-collector/src/test/java/postcollector/presentation/postcollector/cs/CsPostTest.java
@@ -0,0 +1,37 @@
+package postcollector.presentation.postcollector.cs;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CsPostTest {
+
+    private CsPost normalPost;
+    private CsPost noticePost;
+
+    @BeforeEach
+    void setUp() {
+        normalPost = new CsPost("2345", "게시글 제목",
+            "http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx=29596&page=2&code=notice",
+            "21.11.24");
+        noticePost = new CsPost("[공지]", "게시글 제목",
+            "http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx=29596&page=2&code=notice",
+            "21.11.24");
+    }
+
+    @DisplayName("게시글 번호에 '[공지]'라고 적혀있을 경우에만 참을 반환한다.")
+    @Test
+    void isNotNotice() {
+        assertThat(normalPost.isNotNotice()).isTrue();
+        assertThat(noticePost.isNotNotice()).isFalse();
+    }
+
+    @DisplayName("게시글 번호에 '[공지]'라고 적혀있을 경우 참을 반환한다.")
+    @Test
+    void isNotice() {
+        assertThat(normalPost.isNotice()).isFalse();
+        assertThat(noticePost.isNotice()).isTrue();
+    }
+}
