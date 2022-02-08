9391a68dcf69b9e2a11b7a62e7bcdcc422b2e9a3
ROOT <junroot0909@gmail.com>
2022-02-08 22:39:47 +0900

    fix: 게시글 수집 메타데이터의 수집 시간이 기록되지 않는 버그 수정

--- hy-notice/hy-notice-client/src/main/java/hynotice/postcollector/PostCollectorApplication.java
+++ hy-notice/hy-notice-client/src/main/java/hynotice/postcollector/PostCollectorApplication.java
@@ -3,10 +3,12 @@ package hynotice.postcollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaAuditing
 @EntityScan("hynotice")
 @EnableJpaRepositories(basePackages = "hynotice")
 @SpringBootApplication(scanBasePackages = "hynotice")
