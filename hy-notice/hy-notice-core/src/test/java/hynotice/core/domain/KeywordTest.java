341326641308355cd665a37e58d04a22be5628ab
ROOT <junroot0909@gmail.com>
2022-05-16 16:00:02 +0900

    test: 키워드 관련 테스트 케이스 추가

--- hy-notice/hy-notice-core/src/test/java/hynotice/core/domain/KeywordTest.java
+++ hy-notice/hy-notice-core/src/test/java/hynotice/core/domain/KeywordTest.java
@@ -5,14 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hynotice.core.exception.ExpectedException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class KeywordTest {
 
     @DisplayName("잘못된 키워드를 입력하면 예외가 발생한다.")
-    @ValueSource(strings = {"공 백", " 공백", "공백 ", "@특수문자", "점.점"})
+    @ValueSource(strings = {"", "공 백", " 공백", "공백 ", "@특수문자", "점.점"})
     @ParameterizedTest
     void InvalidKeywordTest(final String value) {
         assertThatThrownBy(() -> new Keyword(value)).isInstanceOf(ExpectedException.class);
