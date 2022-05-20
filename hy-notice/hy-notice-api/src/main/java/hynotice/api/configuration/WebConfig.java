88e4c3f3c0cb88521a78d8e5202e853e28e6e17c
ROOT <junroot0909@gmail.com>
2022-05-20 23:54:41 +0900

    feat: cors 문제 해결

---
+++ hy-notice/hy-notice-api/src/main/java/hynotice/api/configuration/WebConfig.java
@@ -0,0 +1,15 @@
+package hynotice.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("*");
+    }
+}
